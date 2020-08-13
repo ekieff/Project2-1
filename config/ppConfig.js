d60af8852dea423da52256d14a7510dd0c169d80
imjchiang <jchiang296@gmail.com>
2020-08-13 14:34:38 -0700

    [passport] add ppConfig

---
+++ config/ppConfig.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+//passport serializes youur info make it easier to login
+
+passport.serializeUser((user, callback) =>
+{
+    callback(null, user.id);
+});
+
+passport.deserializeUser((id, callback) =>
+{
+    callback(null, id)
+    .catch(callback());
+});
+
+passport.use(new localStrategy(
+{
+    usernameField: "email",
+    passwordField: "password"
+}, 
+(email, password, callback) =>
+{
+    db.user.findOne(
+    {
+        where: 
+        {
+            email
+        }
+    })
+    .then(user =>
+    {
+        if (!user || !user.validPassword(password))
+        {
+            callback(null, false);
+        }
+        else
+        {
+            callback(null, user);
+        }
+    })
+    .catch(callback());
+}))
