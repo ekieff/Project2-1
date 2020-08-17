9613302fbd54fe806264fd1088b5a7de0694492b
imjchiang <jchiang296@gmail.com>
2020-08-17 01:30:15 -0700

    [oneChamp] create route for specific champ, display name, add catch for errors

--- routes/champs.js
+++ routes/champs.js
@@ -16,9 +16,36 @@ router.get("/", function(req, res)
     {
         let allChamps = data.data;
         let allChampNames = Object.getOwnPropertyNames(allChamps);
-        console.log(allChampNames);
+        //console.log(allChampNames);
         res.render("lol/champs", {allChamps:allChamps, allNames:allChampNames});
     })
-})
+    .catch(err =>
+    {
+        console.log("ERROR: FETCH CALL FOR ALL CHAMPS DISPLAY", err);
+        res.send("ERROR: FETCH CALL FOR ALL CHAMPS DISPLAY");
+    });
+});
+
+router.get("/:name", function(req, res)
+{
+    fetch("http://ddragon.leagueoflegends.com/cdn/10.16.1/data/en_US/champion.json")
+    .then(response =>
+    {
+        return response.json();
+    })
+    .then(data =>
+    {
+        let paramName = req.params.name;
+        let champName = paramName.slice(0, 1) + paramName.slice(1, paramName.length);
+        let champ = data.data[champName];
+        //console.log(champ);
+        res.render("lol/oneChamp", {champ});
+    })
+    .catch(err =>
+    {
+        console.log("ERROR: FETCH CALL FOR ONE CHAMP DISPLAY", err);
+        res.send("ERROR: FETCH CALL FOR ONE CHAMP DISPLAY");
+    });
+});
 
 module.exports = router;
